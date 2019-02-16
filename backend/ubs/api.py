9194c2312157c3ae394852f1c033fc303fd7e79d
Bruno Frare <bruno.frare@geoambiente.com.br>
2019-02-16 10:28:28 -0200

    fix import

---
+++ backend/ubs/api.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, include
+from rest_framework import routers, serializers, viewsets
+from ubs.models import UBS
+
+
+class UbsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UBS
+        fields = ('nom_estab')
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = UBS.objects.all()
+    serializer_class = UbsSerializer
+
+
--- backend/ubs/management/commands/sample.py
+++ backend/ubs/management/commands/sample.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 import requests
+from ubs.models import UBS
 
 class Command(BaseCommand):
     help = "My shiny new management command."
@@ -10,4 +11,9 @@ class Command(BaseCommand):
             r = requests.get('https://api-ldc-hackathon.herokuapp.com/api/ubs/' + str(i))
 
             response = r.json()
-            
+            ubs = response.records
+            for u in ubs:
+                model_ubs = UBS()
+                model_ubs.nom_estab = u.nom_estab
+
+                model_ubs.save()
