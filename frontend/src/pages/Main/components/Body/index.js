d3f2c875e9008995c0db2be7979fedc507804b1b
fedsp <roniroman@live.com>
2019-02-16 13:17:16 -0200

    Tabs

--- frontend/src/pages/Main/components/Body/index.js
+++ frontend/src/pages/Main/components/Body/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Container, Form, InputText, Button, FormHeader, List } from './styles';
 import axios from "axios";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
 
 class Body extends Component {
   state = {
@@ -10,17 +12,17 @@ class Body extends Component {
   }
 
   componentDidMount() {
-    this.setState({ cities: [{ name: 'SJC', cod: 1 }, { name: 'JAC', cod: 2 }]})
+    this.setState({ cities: [{ name: 'SJC', cod: 1 }, { name: 'JAC', cod: 2 }] })
   }
   onChangeCity = (e) => {
     this.setState({ city: e.target.value })
   }
 
-  doSearch = () => {    
-    const url = "http://api-ldc-hackathon.herokuapp.com/api/ubs/city/";    
+  doSearch = () => {
+    const url = "http://api-ldc-hackathon.herokuapp.com/api/ubs/city/";
     axios.post(url, { city: this.state.city, page: 1 }, { "Access-Control-Allow-Origin": true })
       .then(response => {
-        this.setState({ubss:response.data.records})
+        this.setState({ ubss: response.data.records })
       }
       ).catch(error => {
         console.log(error)
@@ -30,14 +32,28 @@ class Body extends Component {
   render() {
     return (
       <Container>
-        <Form>
-          <FormHeader className="title1">Busca por nome de cidade</FormHeader>
-          <InputText type="text" value={this.state.city} onChange={e => this.setState({ city: e.target.value })} />
-          <Button onClick={this.doSearch} >Buscar</Button>
-          <List>
-            {this.state.ubss.map(item=><li>{item.nom_estab}, {item.dsc_endereco}, {item.co_cep}</li>)}
-          </List>
-        </Form>
+        <Tabs>
+          <TabList>
+            <Tab>Title 1</Tab>
+            <Tab>Title 2</Tab>
+          </TabList>
+
+          <TabPanel>
+            <Form>
+              <FormHeader className="title1">Busca por nome de cidade</FormHeader>
+              <InputText type="text" value={this.state.city} onChange={e => this.setState({ city: e.target.value })} />
+              <Button onClick={this.doSearch} >Buscar</Button>
+              <List>
+                {this.state.ubss.map(item => <li>{item.nom_estab}, {item.dsc_endereco}, {item.co_cep}</li>)}
+              </List>
+            </Form>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
+
+
       </Container>
 
       //Tela 1 (pesquisa por cidade)
--- frontend/src/pages/Main/components/Body/styles.js
+++ frontend/src/pages/Main/components/Body/styles.js
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   padding: 2rem;
+
+  ul{
+    font-size:2rem;
+  }
 `
 
 export const Form = styled.div`
